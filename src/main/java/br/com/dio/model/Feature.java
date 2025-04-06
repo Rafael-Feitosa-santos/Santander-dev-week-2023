c9c6ac07bd025c6b80b72d4815a1544c0f2b323e
Rafael Feitosa <rafaelfeit@hotmail.com>
2025-04-06 13:34:05 -0300

    Initial commit

---
+++ src/main/java/br/com/dio/model/Feature.java
@@ -0,0 +1,9 @@
+package br.com.dio.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_feature")
+public class Feature extends BaseItem{
+}
---
+++ src/main/java/br/com/dio/model/News.java
@@ -0,0 +1,9 @@
+package br.com.dio.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_news")
+public class News extends BaseItem {
+}
