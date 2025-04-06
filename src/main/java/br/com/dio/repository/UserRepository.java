ae2eda95ec698a901d43e043671dcb70269a887c
Rafael Feitosa <rafaelfeit@hotmail.com>
2025-04-06 13:40:38 -0300

    inclusão o package repository

---
+++ src/main/java/br/com/dio/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package br.com.dio.repository;
+
+import br.com.dio.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
